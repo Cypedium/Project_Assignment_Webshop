5f806856ff719fabf544a3e614903f09e7135dc5
Mikael Aurell <aurell.mikael@gmail.com>
2020-05-04 15:10:23 +0200

    Added pictures to List

--- Project_Assignment_Webshop/ClientApp/src/components/List.js
+++ Project_Assignment_Webshop/ClientApp/src/components/List.js
@@ -1,28 +1,33 @@
 import React, { Component, Fragment } from 'react';
+import salami from './pictures/salami.jpg';
 
 export default class List extends Component {  
      
         render() {  
             const TableHeader = props => {
                 return (
-                    <tr>
+                    <tr> 
                         <th>
-                            <button className="btn btn-link m-2" onClick={() => props.sortByInt("ProductType")}>ProductType</button>
+                            {/* Hiding
+                            <button className="text-left btn btn-link" onClick={() => props.sortByInt("ProductType")}>ProductType</button>
+                            */}
+                        </th>
+                        
+                        <th>
+                            <button className="text-left btn btn-link" onClick={() => props.sortByInt("Number")}>Number</button>
                         </th>
                         <th>
-                            <button className="btn btn-link m-2" onClick={() => props.sortByInt("Number")}>Number</button>
+                            <button className="text-left btn btn-link" onClick={() => props.sortByString("Name")}>Name</button>
                         </th>
                         <th>
-                            <button className="btn btn-link m-2" onClick={() => props.sortByString("Name")}>Name</button>
+                            <button className="text-left btn btn-link" onClick={() => props.sortByString("Description")}>Description</button>
                         </th>
+                        <th></th>
                         <th>
-                            <button className="btn btn-link m-2" onClick={() => props.sortByString("Description")}>Description</button>
-                        </th>
-                        <th>
-                            <button className="btn btn-link m-2" onClick={() => props.sortByInt("Price")}>Price</button>
+                            <button className="text-left btn btn-link" onClick={() => props.sortByInt("Price")}>Price</button>
                         </th> 
                         <th>
-                            <div className="m-2">Options</div>
+                            <div className="text-left"></div>
                         </th>                     
                     </tr>
                 );  
@@ -32,39 +37,42 @@ export default class List extends Component {
 
             const TableBody = props => {
                 const rows = props.productList.map((row) => {
-                    return (                    
-                        <tr key={"ProductId" + row.Id}>
-                            <td>{row.ProductType}</td>
-                            <td>{row.Number}</td>
-                            <td>{row.Name}</td>
-                            <td>{row.Description}</td>
-                            <td>{row.Price}</td>
-                            <td>
-                                <button className="btn btn-primary" onClick={() => props.detailProduct(row.Id)}>Detail</button>
-                                {" "}
-                                <button className="btn btn-danger" onClick={() => props.removeProduct(row.Id) }>Delete</button>
-                                {" "}
-                                <button className="btn btn-success" onClick={() => props.addProductToProductt(row.Id) }>AddToCart</button>                                   
-                            </td>                            
-                        </tr>                    
-                    )
+                    return (
+                            <tr key={"ProductId" + row.Id}>
+                                <td><img src={salami} height="25%" width="25%" /></td>
+                                <td>{/*--Hiding--{row.ProductType}--*/}</td>
+                                <td>{row.Number}</td>
+                                <td>{row.Name}</td>
+                                <td>{row.Description}</td>
+                                <td></td>
+                                <td>{row.Price}</td>
+                                
+                                <td>
+                                    <button className="btn btn-primary m-2" onClick={() => props.detailProduct(row.Id) }>Detail</button>
+                                
+                                    <button className="btn btn-danger m-2" onClick={() => props.removeProduct(row.Id) }>Delete</button>
+                                
+                                    <button className="btn btn-success m-2" onClick={() => props.addProductToProductt(row.Id) }>AddToCart</button>                                   
+                                </td>                            
+                            </tr>                   
+                            )
                 });
-                return <tbody>{rows}</tbody>
+                return <tbody className="table table-striped">{rows}</tbody>
             }
             return (                
-                <Fragment>
-                    <TableHeader
-                        sortByInt={this.props.sortByInt}
-                        sortByString={this.props.sortByString}
-                    />
-                    <TableBody
-                        productList={this.props.productList}
-                        detailProduct={this.props.detailProduct}
-                        removeProduct={this.props.removeProduct}
-                        addProductToProduct={this.props.addProductToProduct}
-                    />
-                </Fragment>
-            );
+                        <Fragment>
+                            <TableHeader
+                                sortByInt={this.props.sortByInt}
+                                sortByString={this.props.sortByString}
+                            />
+                            <TableBody
+                                productList={this.props.productList}
+                                detailProduct={this.props.detailProduct}
+                                removeProduct={this.props.removeProduct}
+                                addProductToProduct={this.props.addProductToProduct}
+                            />
+                        </Fragment>
+                    );
         }
     
 }
